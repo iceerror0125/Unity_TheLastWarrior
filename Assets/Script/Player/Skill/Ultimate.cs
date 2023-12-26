2398f0a28f7725f613314ebcf49082ba6fdcd1c6
GiaHung5121 <truongtrangiahung@gmail.com>
2023-12-26 20:40:28 +0700

    Ultimate

--- Assets/Script/Player/Skill/Ultimate.cs
+++ Assets/Script/Player/Skill/Ultimate.cs
@@ -2,17 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ultimate : MonoBehaviour
+public class Ultimate : PlayerSkill
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // Player's velocity is zero while activating skill.
+    // Skill will be in front of player (left and right)
+    [SerializeField] private GameObject skillPrefab;
+    [SerializeField] private float x;
 
-    // Update is called once per frame
-    void Update()
+    private UltimateSetUp script;
+
+    protected override void SetUp()
     {
-        
+        base.SetUp();
+
+        Vector2 spawnPos = new Vector2 (player.transform.position.x + player.EntityDir() * x, player.transform.position.y);
+        GameObject skill = Instantiate(skillPrefab, spawnPos, Quaternion.identity); 
+        script = skill.GetComponent<UltimateSetUp>();
+        if (player.EntityDir() < 0 )
+        {
+            script.ReverseRotation();
+        }
+        script.Activate();
+    }
+    protected override void ToExitCodition()
+    {
+        StartCoroutine(ToExit());
+    }
+    private IEnumerator ToExit()
+    {
+        yield return new WaitForSeconds(duration);
+        script.DestroySkill();
+        base.ToExitCodition();
     }
 }
---
+++ Assets/Script/Player/Skill/UltimateSetUp.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UltimateSetUp : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float countdown;
+    [SerializeField] private float damageCountdown;
+
+    private Vector3 scaleChange;
+    private float scaleTimer;
+    public Vector3 size;
+    void Start()
+    {
+        transform.localScale = new Vector3(transform.localScale.x, 0.01f, transform.localScale.z);
+        scaleChange = new Vector3(0, speed, 0);
+        scaleTimer = countdown;
+    }
+
+    void Update()
+    {
+        scaleTimer -= Time.deltaTime;
+       
+        Action();
+    }
+    private void Action()
+    {
+        if (scaleTimer < 0)
+        {
+            if (transform.localScale.y < 1)
+                transform.localScale += scaleChange;
+        }
+    }
+    public void ReverseRotation()
+    {
+        transform.Rotate(0, 180, 0);
+    }
+    public void Activate()
+    {
+        Start();
+        Update();
+    }
+    public void DestroySkill()
+    {
+        Destroy(gameObject);
+    }
+    
+    public void CauseDamage()
+    {
+        if (transform.localScale.y < 0.99)
+            return;
+
+        var colliders = Physics2D.OverlapBoxAll(transform.position, size, 0);
+        foreach (var collider in colliders)
+        {
+            Enemy enemy = collider.GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                PlayerManager.instance.player.SpellHit(enemy, PlayerManager.instance.player.stat.Damage);
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawCube(transform.position, size);
+    }
+}
